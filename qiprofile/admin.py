cf8383fd96b12e95b471f4321d02c1de314c4c9b
FredLoney <loneyf@ohsu.edu>
2014-02-14 13:59:40 -0800

    Subject is the only admin object.

--- qiprofile/admin.py
+++ qiprofile/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import (Project, Collection)
+from .models import (Subject)
 
 # Register the objects on the admin page.
-admin.site.register(Project)
-admin.site.register(Collection)
+admin.site.register(Subject)

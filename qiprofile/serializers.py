e8f7774e56916d88355ec829079b663ab948e179
FredLoney <loneyf@ohsu.edu>
2014-02-14 14:01:41 -0800

    Add the SubjectDetail.

--- qiprofile/serializers.py
+++ qiprofile/serializers.py
@@ -1,18 +1,12 @@
-from rest_framework import serializers as ser
-from .models import (Collection, Subject, Session, Modeling, TNM,
-                     NottinghamGrade, HormoneReceptorStatus,
-                     BreastPathology, SarcomaPathology)
+from rest_framework import serializers
+from .models import (Subject, SubjectDetail)
 
 
-class CollectionSerializer(ser.ModelSerializer):
-    subjects = ser.HyperlinkedIdentityField('subjects', view_name='subjects', lookup_field='name')
-    
-    class Meta:
-        model = Collection
-        fields = ('id', 'name')
-
-
-class SubjectSerializer(ser.ModelSerializer):
+class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
-        fields = ('id', 'name')
+
+
+class SubjectDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectDetail

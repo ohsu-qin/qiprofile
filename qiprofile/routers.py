9d2bb7a6c8923961c3fc21e002e2db5a46fdb4f2
FredLoney <loneyf@ohsu.edu>
2014-02-20 15:30:42 -0800

    Split out the qiprofile-rest package.

--- qiprofile/routers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from .views import (SubjectViewSet, SubjectDetailViewSet)
-
-router = routers.SimpleRouter()
-router.register(r'subjects', SubjectViewSet)
-router.register(r'subject-detail', SubjectDetailViewSet)

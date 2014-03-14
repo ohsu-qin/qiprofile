67739215f605ffe21bff90140a56415902df8b3e
FredLoney <loneyf@ohsu.edu>
2014-03-13 17:12:34 -0700

    Fix doc.

---
+++ doc/source/conf.py
@@ -0,0 +1,14 @@
+import os
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo']
+autoclass_content = "both"
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'qiprofile'
+copyright = u'2014, OHSU Knight Cancer Institute'
+pygments_style = 'sphinx'
+html_theme = 'theme'
+html_theme_path = ['.']
+html_theme_options = dict(linkcolor='DarkSkyBlue', visitedlinkcolor='Navy')
+htmlhelp_basename = 'qiprofilerestdoc'
+html_title = "qiprofile"

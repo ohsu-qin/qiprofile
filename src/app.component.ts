8fa922eba9bdea561fc00a96c94417456cfe0da8
FredLoney <loneyf@ohsu.edu>
2016-06-14 09:52:35 -0700

    Add ng2-resource-rest.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,9 @@
 import '../shims.js';
 import { Component } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { RESOURCE_PROVIDERS } from 'ng2-resource-rest';
+
 import { CollectionsComponent } from './collections.component.ts';
 
 @Component({
@@ -11,4 +14,4 @@ import { CollectionsComponent } from './collections.component.ts';
 
 class AppComponent { }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HTTP_PROVIDERS, RESOURCE_PROVIDERS]);

0ecd312bd1430f7a8733f61f47258a894589eea7
FredLoney <loneyf@ohsu.edu>
2016-06-14 15:40:13 -0700

    Remove unused import.

--- src/rest.resource.ts
+++ src/rest.resource.ts
@@ -1,4 +1,4 @@
-import { Resource, ResourceParams, ResourceResult } from 'ng2-resource-rest';
+import { Resource, ResourceParams } from 'ng2-resource-rest';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as _ from 'lodash';

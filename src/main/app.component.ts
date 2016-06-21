7263a4f4ed03ae7ad2cb4413e890c416b100f957
FredLoney <loneyf@ohsu.edu>
2016-06-21 09:51:09 -0700

    Add the app component.

---
+++ src/main/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit} from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, Routes, ROUTER_DIRECTIVES } from '@angular/router';
+
+import { CollectionsComponent } from '../collections/collections.component.ts';
+import { HelpService } from '../common/help.service.ts';
+
+@Component({
+  selector: 'qi-app',
+  templateUrl: '/public/html/main/app.html',
+  directives: [ROUTER_DIRECTIVES],
+  // The HelpService singleton is shared by subcomponents, which
+  // should not declare the provider separately.
+  providers: [HelpService]
+})
+
+@Routes([
+  {path: '/qiprofile/:project', component: CollectionsComponent}
+])
+
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private location: Location) { }
+
+  ngOnInit() {
+    // Kick the router to get things started.
+    this.router.navigateByUrl(this.location.path());
+  }
+}

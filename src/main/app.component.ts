149bb5aef4ddec86365a8fd1827ad1f79bb9326a
FredLoney <loneyf@ohsu.edu>
2016-06-22 10:00:08 -0700

    Add space.

--- src/main/app.component.ts
+++ src/main/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, Routes, ROUTER_DIRECTIVES } from '@angular/router';
 

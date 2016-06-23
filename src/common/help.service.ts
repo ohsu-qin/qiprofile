03d14aa755e08655af7e09b8071abdc2d65b4a69
FredLoney <loneyf@ohsu.edu>
2016-06-23 15:30:18 -0700

    Add method doc.

--- src/common/help.service.ts
+++ src/common/help.service.ts
@@ -2,13 +2,27 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 
+/**
+ * This service encapsulates the `showHelp` flag.
+ *
+ * @class HelpService
+ */
 export class HelpService {
+  /**
+   * @property showHelp flag indicating whether to display
+   *   the help pane
+   */
   showHelp: boolean;
   
   constructor() {
      this.showHelp = false;
   }
 
+  /**
+   * Inverts the `showHelp` flag.
+   *
+   * @method toggleHelp
+   */
   toggleHelp() {
     this.showHelp = !this.showHelp;
   }

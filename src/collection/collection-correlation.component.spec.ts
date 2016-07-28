817e51ab017edebfbf493e7bace90dd93e4bc34b
FredLoney <loneyf@ohsu.edu>
2016-07-28 11:34:55 -0700

    Add correlation component.

---
+++ src/collection/collection-correlation.component.spec.ts
@@ -0,0 +1,26 @@
+//import { Observable } from 'rxjs';
+//import { provide } from '@angular/core';
+import {
+  describe, it, expect
+} from '@angular/core/testing';
+
+import { CollectionCorrelationComponent } from './collection-correlation.component.ts';
+
+/**
+ * {{#crossLink "CollectionCorrelationComponent"}}{{/crossLink}} validator.
+ *
+ * @module collection
+ * @class CollectionCorrelationComponentSpec
+ */
+describe('The CollectionCorrelation component', function() {
+  let component;
+
+  beforeEach(() => {
+    component = new CollectionCorrelationComponent();
+  });
+  
+  it('should convert the config to a chart config', function() {
+     component.config = {foo: 'bar'};
+     expect(component.config, "Config is incorrect").to.eql({foo: 'bar'});
+  });
+});

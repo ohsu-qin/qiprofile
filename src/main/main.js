65a522a0f9f4801209b54aa0cc62d4d23191e831
FredLoney <loneyf@ohsu.edu>
2016-08-04 09:35:12 -0700

    Cosmetic change.

--- src/main/main.js
+++ src/main/main.js
@@ -6,6 +6,7 @@ import { RESOURCE_PROVIDERS } from 'ng2-resource-rest';
 
 import { AppComponent } from './app.component.ts';
 
-bootstrap(AppComponent,
-          [APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, RESOURCE_PROVIDERS]
+bootstrap(
+  AppComponent,
+  [APP_ROUTER_PROVIDERS, HTTP_PROVIDERS, RESOURCE_PROVIDERS]
 ).catch(err => console.error(err));

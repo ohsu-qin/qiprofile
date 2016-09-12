441bc0a77c2354b8d8957391b03f94361c0022c9
FredLoney <loneyf@ohsu.edu>
2016-09-12 11:15:24 -0700

    Move directives to module.

--- src/page/page.component.ts
+++ src/page/page.component.ts
@@ -1,38 +1,12 @@
 /**
- * The Page abstraction module.
- *
- * @module page
- * @main page
- */
-import { HomeComponent } from '../home/home.component.ts';
-import { ToggleHelpComponent } from '../help/toggle-help.component.ts';
-import { HelpComponent } from '../help/help.component.ts';
-import { ErrorComponent } from '../error/error.component.ts';
-
-/**
  * The abstract page component base class. A *page component* is
  * a component with a distinct url location and HTML page.
  *
- * 
- *
  * @class PageComponent
+ * @module page
  * @abstract
  */
 export abstract class PageComponent {
-  /**
-   * The standard page directives. The directives consist of the following:
-   * * {{#crossLink "HomeComponent"}}{{/crossLink}}
-   * * {{#crossLink "ToggleHelpComponent"}}{{/crossLink}}
-   * * {{#crossLink "HelpComponent"}}{{/crossLink}}
-   * * {{#crossLink "ErrorComponent"}}{{/crossLink}}
-   *
-   * @property DIRECTIVES {class[]}
-   * @static
-   */
-  static DIRECTIVES = [
-    HomeComponent, ToggleHelpComponent, HelpComponent, ErrorComponent
-  ];
-
   /**
    * The help content.
    *
@@ -46,7 +20,7 @@ export abstract class PageComponent {
    * @property error {string}
    */
   error: string;
-  
+
   /**
    * The help string is fed into the help pull-down when the help button
    * is clicked. The standard mechanism is the `qi-help` directive
@@ -61,13 +35,13 @@ export abstract class PageComponent {
   constructor(help?: string) {
     this.help = help;
   }
-  
+
   /**
    * Sets the error property which triggers the error pop-pup.
    * The subclass has the responsibility of binding an error event
    * to this handler, e.g.:
    *
-   *     qi-some-directive((error)="onError($event)") 
+   *     qi-some-directive((error)="onError($event)")
    *
    * @method onError
    * @param message {string} the error messaage

1f01699129b60e1a7ccde1ff4931e53eb8711197
FredLoney <loneyf@ohsu.edu>
2016-09-27 16:26:17 -0700

    Add topic prefix discriminator.

--- src/configuration/configuration.service.ts
+++ src/configuration/configuration.service.ts
@@ -9,6 +9,7 @@ import { Injectable } from '@angular/core';
 import { parse } from 'ini-parser';
 
 import labels from './labels.cfg';
+import properties from './properties.cfg';
 
 @Injectable()
 
@@ -25,41 +26,110 @@ export class ConfigurationService {
    */
   labels: Object;
 
-  constructor() {
-    this.labels = parse(labels);
-  }
-  
   /**
-   * Determines the label for the given attribute, as follows:
-   * * If there is an entry for the attribute in the `labels.cfg`
-   *   file, then that configuration entry value is the label
-   * * Otherwise, the label is the capitalized, space-separated
-   *   parse of the attribute, e.g. `tumorSize` => `Tumor Size`.
+   * The parsed properties configuration.
    *
-   * The label look-up can be qualified by the configuration
-   * file section. If the section is not supplied, then all
-   * sections are checked.
+   * @property properties
+   */
+  properties: Object;
+
+  constructor() {
+    // TODO - merge an admin-defined server file path.
+    this.labels = parse(labels);
+    this.properties = parse(properties);
+  }
+
+  /**
+   * Returns the properties and property paths listed in
+   * the properties configuration under the given topic.
+   *
+   * @method getProperties
+   * @param topic {string} the topic to search
+   * @return {string[]} the topic properties, or null if none
+   */
+  getProperties(topic: string) {
+    return this.properties[topic];
+  }
+
+  /**
+   * Determines the label for the given property as follows:
+   * * If the topic is provided, then the label is looked up
+   *   in the `labels.cfg` file.
+   * * Otherwise, the right-most item in the property path with
+   *   a comparable topic in `labels.cfg` is used, e.g. if
+   *   the path is `subject.biopsy.pathology.tnm.tumorSize`
+   *   then a `[Pathology]` topic would scope the subpath
+   *  `tnm.tumorSize`.
+   * * If no topic could be determined, then all topics in
+   *   the configuration file are consulted.
+   *
+   * The label match for a given topic is determined as follows:
+   * * If there is an entry for the property within the topic, then
+   *   that configuration entry value is the label.
+   * * Otherwise, the label is the capitalized, space-separated
+   *   lookup of each item in the property path, e.g.
+   * * If there is no configuration match, then the default is
+   *   the capitalized, space-separated "humanized" parse of the
+   *   property, e.g. `tumorSize` => `Tumor Size`.
+   *
+   * The label match is applied element-wise in the property path,
+   * e.g. if there is a `[Pathology]` `tnm = TNM` entry, then the
+   * label for path `subject.biopsy.pathology.tnm.tumorSize` is
+   * `TNM Tumor Size`.
    *
    * @method getLabel
-   * @param {string} attribute
-   * @param {string} the optional label config section, e.g. `Modeling`
-   * @return {string} the attribute label
+   * @param property {string} the property name
+   * @param topic {string} the optional configuration topic
+   * @return {string} the property label
    */
-  getLabel(attribute: string, section?: string): string {
-    if (section) {
-      section = _s.capitalize(section);
-      let label = this.labels[section][attribute];
-      if (label) { return label; }
+  getLabel(property: string, topic?: string): string {
+    let path = property.split('.');
+    if (topic) {
+      return this.getPathLabel(path, topic);
+    } else {
+      let caps = path.map(_.s.capitalize);
+      let topicNdx = _.findLastIndex(caps, s => s in this.labels);
+      if (topicNdx === -1) {
+        return getPathLabel(path);
       } else {
-      for (let lblSection in this.labels) {
-        let label = this.labels[lblSection][attribute];
-        if (label) { return label; }
+        let prefix = _.take(caps, topicNdx + 1).join(' ');
+        topic = caps[topicNdx];
+        let subpath = path.slice(topicNdx + 1).join('.');
+        let suffix = this.getPathLabel(subpath, topic);
+        return `${ prefix } ${ suffix }`;
       }
     }
-    return this.defaultLabelFor(attribute);
   }
-  
-  private defaultLabelFor(attribute: string): string {
-    return _s.humanize(attribute).split(' ').map(_s.capitalize).join(' ');
+
+  private getPathLabel(path: string[], topic?: string): string {
+    // If no topic given, then check all topics.
+    let topics = topic ? [topic] : this.labels.keys();
+    let labels = path.map(prop => this.getConfigLabel(prop, topics));
+
+    return labels.join(' ');
+  }
+
+  private getConfigLabel(property: string, topics: string[]): string {
+    // Look in each relevant topic for the label.
+    for (topic in topics) {
+      let label = this.labels[topic][property];
+      if (label) {
+        return label;
+      }
+    }
+    // Not found: make the default label.
+    return this.defaultLabel(property);
+  }
+
+  /**
+   * Returns the default property label as described in
+   * {{#crossLink "ConfigurationService/getLabel"}}{{/crossLink}}.
+   *
+   * @method defaultLabel
+   * @param {string} the property name
+   * @return {string} the default property label
+   */
+  private defaultLabel(property: string): string {
+    return _s.humanize(property).split(' ').map(_s.capitalize).join(' ');
   }
 }

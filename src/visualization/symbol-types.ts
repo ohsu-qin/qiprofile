710343dbb7fac5865910fb38b726b1c186927e30
FredLoney <loneyf@ohsu.edu>
2017-01-20 14:58:38 -0800

    Remove unused files.

--- src/visualization/symbol-types.ts
+++
@@ -1,9 +0,0 @@
-import * as d3 from 'd3';
-
-// The D3 v4 symbols
-// (cf. https://github.com/d3/d3-shape/blob/master/README.md#symbols).
-// The symbols are sorted by inverse preference.
-export const SYMBOL_TYPES = [
-  d3.symbolCircle, d3.symbolDiamond, d3.symbolStar, d3.symbolTriangle,
-  d3.symbolSquare, d3.symbolCross, d3.symbolWye
-];
